687ab55db88bf4b3b26599de2229adea262f4f1c
Stina Persson <stpe1901@student.miun.se>
2025-04-07 10:08:51 +0200

    rutt för portfolio och tjänster skapad

--- routes/portfolio.route.js
+++ routes/portfolio.route.js
@@ -1,24 +1,103 @@
+const Joi = require('joi');
 const Portfolio = require('../models/portfolio');
 const Joi = require('joi');
 
 const portfolioRouteArr = [
     {
-        method: 'GET', 
-    }, 
-
-    {
+        //hämtar hela protfoliolistan
         method: 'GET',
+        path: '/portfolio', 
+        handler: async (request, h) => {
+            try {
+                const portfolio = await Portfolio.find();
+                return h.response(portfolio).code(200);
+            } catch(error) {
+                return h.response({ error: "Failed to fetch portfolio"}).code(500); 
+            }
+        }
     }, 
 
     {
+        //hämtar ett projekt från portfolion på id 
+        method: 'GET',
+        path: '/portfolio/{id}', 
+        handler: async (request, h) => {
+            try {
+                const portfolio = await Portfolio.findById(request.params.id); 
+                h.response(portfolio).code(200); 
+            } catch(error) {
+                return h.response({error: "Failed to fetch single project"}).code(500); 
+            }
+        }
+    }, 
+
+    {
+        //lägget till nytt porjekt till portfolion
         method: 'POST',
+        path: 'portfolio',
+        options: {
+            validate: {
+                payload: Joi.object({
+                    project_name: Joi.string().min(3).required(), 
+                    project_description: Joi.string().min(3).required(),
+                })
+            }
+        },
+        handler: async (request, h) => {
+            try {
+                const portfolio = new Portfolio(request.payload); 
+                return await portfolio.save(); 
+            } catch(error) {
+                return h.response({error: "Failed to add new project to portfolio"}).code(500); 
+            }
+        }
     }, 
 
     {
+        //Uppdaterar projekt i portfolion 
         method: 'PUT',
+        path: 'portfolio/{id}', 
+        options: {
+            validate: {
+                payload: Joi.object({
+                    project_name: Joi.string().min(3).required(), 
+                    project_description: Joi.string().min(3).required(),
+                })
+            }
+        },
+        handler: async (request, h) => {
+            try {
+                const portfolio = Portfolio.findByIdAndUpdate(
+                    request.params.id, 
+                    request.payload, 
+                    {new: true}
+                ); 
+                if(!portfolio) { 
+                    return h.response({error: "Project not found in portfolio"}).code(404); 
+                }
+                return h.response(portfolio).code(200); 
+            } catch {
+                return h.response({error: "Failed to update project in portfolio"}).code(500); 
+            }
+        }
     }, 
 
     {
+        //Tar bort ett projekt från portfolion
         method: 'DELETE',
+        path: 'portfolio/{id}', 
+        handler: async (request, h) => {
+            try {
+                const portfolio = await Portfolio.findByIdAndDelete(request.params.id); 
+                if(!portfolio) {
+                    return h.response({error: "Project not found in portfolio"}); 
+                }
+                return h.response({message: "Project deleted successfully"}).code(200); 
+            } catch(error) {
+                return h.response({error: "Failed to delete project from portfolio"})
+            }
+        }
     }
 ]
+
+module.exports = portfolioRouteArr; 
--- routes/services.route.js
+++ routes/services.route.js
@@ -1,24 +1,101 @@
-const Service = require('../models/service');
+const Service = require('../models/services');
 const Joi = require('joi');
 
 const serviceRouteArr = [
     {
         method: 'GET', 
+        path: '/service', 
+        handler: async (request, h) => {
+            try {
+                const service = await Service.find(); 
+                return h.response(service).code(200); 
+            } catch(error) {
+                return h.response({error: "Failed to fetch services"}).code(500); 
+            }
+        }
     }, 
 
     {
         method: 'GET',
+        path: '/service/{id}', 
+        handler: async (request, h) => {
+            try {
+                const service = await Service.findById(request.params.id); 
+                return h.response(service).code(200); 
+            } catch(error) {
+                return h.response({error: "Failed to fetch single service"}).code(500); 
+            }
+        }
     }, 
 
     {
         method: 'POST',
+        path: '/service',
+        options: {
+            validate: {
+                payload: Joi.object({
+                    service_name: Joi.string().min(3).required(), 
+                    service_description: Joi.string().min(3).required(), 
+                })
+            }
+        },
+        handler: async (request, h) => {
+            try {
+                const service = new Service(request.payload)
+                return await service.save(); 
+            } catch(error) {
+                return h.response({error: "Failed to post new service"}).code(500); 
+            }
+        }
     }, 
 
     {
         method: 'PUT',
+        path: '/service/{id}', 
+        options: {
+            validate: {
+                payload: Joi.object({
+                    service_name: Joi.string().min(3).required(), 
+                    service_description: Joi.string().min(3).required(), 
+                })
+            }
+        },
+        handler: async (request, h) => {
+            try {
+                const service = await Service.findByIdAndUpdate(
+                    request.params.id, 
+                    request.payload, 
+                    {new: true}
+                ); 
+                if(!service) {
+                    return h.response({error: "Failed to find service to update"}).cide(404);
+                }; 
+                return h.response(service).code(200); 
+            } catch(error) {
+                return h.response({error: "Failed to update service"}).code(500);
+            }
+        }
+
     }, 
 
     {
         method: 'DELETE',
+        path: '/service/{id}', 
+        handler: async (request, h) => {
+            try {
+                const service = await Service.findByIdAndDelete(request.params.id); 
+
+                if(!service) {
+                    return h.response({error: "Failed to find service to delete"}).code(404); 
+                };
+
+                return h.response({message: "Successfully deleted service"}).code(200); 
+
+            } catch(error) {
+                return h.response({error: "Failed to delete service"}).code(500); 
+            }
+        }
     }
 ]
+
+module.exports = serviceRouteArr; 
