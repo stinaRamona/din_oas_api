c5d20c9fff9fdaa5e821864ea64d14dd38253e1a
Stina Persson <stpe1901@student.miun.se>
2025-06-04 13:49:14 +0200

    ändrat host

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost', 
+        host: '0.0.0.0', 
         routes: {
             cors: {
                 origin: ['*']
